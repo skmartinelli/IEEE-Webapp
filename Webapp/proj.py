0c5ad4807f1bdcfa700be9f3df6d5ab02d08e27f
skmartinelli <skm010@ucsd.edu>
2019-11-19 15:44:55 -0800

    what i got to work

---
+++ Webapp/proj.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, url_for, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+        return render_template('home.html')
+
+@app.route('/ctof')
+def render_ctof():
+    return render_template('ctof.html')
+
+
+@app.route('/ctof_result')
+def render_ctof_result():
+        try:
+            ctemp_result = float(request.args['cTemp'])
+            ftemp_result = ctof(ctemp_result)
+            return render_template('ctof_result.html', cTemp=ctemp_result, fTemp=ftemp_result)
+        except ValueError:
+            return "Sorry: something went wrong."
+
+def ctof(ctemp):
+    return (ctemp*9/5)+32
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
