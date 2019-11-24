4fb7c8c01b06b270ea294e5e8bea29a28010adbf
skmartinelli <skm010@ucsd.edu>
2019-11-24 15:41:23 -0800

    needs a template

--- Webapp/proj.py
+++ Webapp/proj.py
@@ -10,9 +10,10 @@ def render_main():
 def render_summary():
     return render_template('summary.html')
 
-@app.route('/summary_result')
+@app.route('/summary', methods=['POST'])
 def render_summary_result():
-    return render_template('summaryresult.html')
+    link = request.form['link']
+    return link
 
 if __name__ == "__main__":
     app.run(port=5000)
