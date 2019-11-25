e584a554d8e405b76ee9d6767e6f2981205ae848
skmartinelli <skm010@ucsd.edu>
2019-11-24 16:47:39 -0800

    we need a summary exclamation point

--- Webapp/proj.py
+++ Webapp/proj.py
@@ -1,20 +1,23 @@
 import os
+#import summary
 from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def render_main():
-        return render_template('home.html')
 
-@app.route('/summary')
-def render_summary():
+@app.route('/')
+def render_summary():    
     return render_template('summary.html')
 
-@app.route('/summary', methods=['POST'])
+@app.route('/', methods=['POST'])
 def render_summary_result():
     link = request.form['link']
+    #summary= getSummary(link)
     return link
+    #ALEX !!! write the getSummary(String link) function and then this will return getSummary(link) instead of just link
+    
 
 if __name__ == "__main__":
     app.run(port=5000)
+
+
 
